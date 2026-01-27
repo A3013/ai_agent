aac0d9d7db76548da98cf68386245b6d7c682f4b
A3013 <abhijeet301303@gmail.com>
2026-01-27 17:54:21 +0000

    added content to limit file read size and improve security checks

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,2 +1,9 @@
+MAX_CHARS = 10000
+import os
+
 def get_file_content(working_directory, file_path):
-    
+    valid_target_dir = os.path.commonpath([working_directory, file_path]) == working_directory
+    if not valid_target_dir:
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    if not os.path.isfile(file_path):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
